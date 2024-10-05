9e10366928de54728bfcc3885f823afe04e9a7d5
masieh <masihdon@hotmail.de>
2024-10-05 17:02:03 +0200

    update linnks in header

--- me/kmom01/view/header.php
+++ me/kmom01/view/header.php
@@ -15,9 +15,9 @@
     <header>
         <nav class="navbar">
             <ul>
-            <li><a href="/dbwebb-kurser/me/kmom01/public/index.php">Home</a></li>
-        <li><a href="/dbwebb-kurser/me/kmom01/public/report.php">Reports</a></li>
-        <li><a href="/dbwebb-kurser/me/kmom01/public/about.php">About</a></li>
+            <li><a href="index.php">Home</a></li>
+        <li><a href="report.php">Reports</a></li>
+        <li><a href="about.php">About</a></li>
             </ul>
         </nav>
     </header>
