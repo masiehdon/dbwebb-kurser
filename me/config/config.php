f025212d399d7b159b82ffab81f7e71326790169
masieh <masihdon@hotmail.de>
2024-10-18 16:51:24 +0200

    change path

--- me/config/config.php
+++ me/config/config.php
@@ -2,7 +2,7 @@
 
 if ($_SERVER['HTTP_HOST'] == 'localhost') {
     // Configuration for localhost
-    $baseUrl = 'http://localhost/dbwebb-kurser/me/kmom01/public/';
+    $baseUrl = 'http://localhost/dbwebb-kurser/me/public/';
 } else {
     // Configuration for live server
     $baseUrl = 'https://www.student.bth.se/~mado22/dbwebb-kurser/me/kmom01/public/';
